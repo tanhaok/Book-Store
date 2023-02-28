446c1cb76616940fd0c9c5705f84d84717239349
tanhao111 <haonguyentan2001@gmail.com>
2023-02-28 19:20:08 +0700

    [Manager] Create bff for manager-ui

---
+++ manager-bff/src/main/java/com/managerbff/managerbff/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.managerbff.managerbff.config;
+
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityWebFilterChain springSecurityFilterChain(@NotNull ServerHttpSecurity http) throws Exception {
+    return http.authorizeExchange().pathMatchers("/**").permitAll().and().build();
+  }
+}
---
+++ manager-bff/src/test/java/com/managerbff/managerbff/ManagerBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managerbff.managerbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

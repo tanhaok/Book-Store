5cf7523042119d7cad6c1347a3aea9cc963b33a5
tanhao111 <haonguyentan2001@gmail.com>
2023-03-05 14:29:48 +0700

    Add swagger for product

--- product/src/main/java/com/bookstore/product/config/SecurityConfig.java
+++ product/src/main/java/com/bookstore/product/config/SecurityConfig.java
@@ -1,4 +1,57 @@
 package com.bookstore.product.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authentication)
+      throws Exception {
+        return authentication.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+      throws Exception {
+        http
+          .authorizeHttpRequests()
+          .anyRequest().permitAll()
+          .and()
+          .cors().disable()
+          .csrf().disable()
+          .formLogin().disable();
+
+        return http.build();
+
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+          .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api-docs/**", "/api-doc-ui");
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedHeaders("*").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
+
 }
